7bb0145996d76c0d3fe301ee8e1a2584bd67f417
wuwx <wuweixin@gmail.com>
2022-11-06 21:52:17 +0800

    wip

--- src/LaravelPlusViewMiddleware.php
+++ src/LaravelPlusViewMiddleware.php
@@ -19,15 +19,15 @@ class LaravelPlusViewMiddleware
     public function handle($request, Closure $next)
     {
         $_format = $request->get('_format') ?: $request->format();
-        $mime = MimeType::getMimeTypes();
-        if ($mime->getMimeTypes($_format)) {
+
+        if (MimeType::get($_format)) {
             View::addExtension("$_format.php", "php");
             View::addExtension("$_format.blade.php", "blade");
         }
 
         $response = $next($request);
 
-        if ($mime->getMimeTypes($_format) && is_a($response, 'Illuminate\Http\Response')) {
+        if (MimeType::get($_format) && is_a($response, 'Illuminate\Http\Response')) {
             $original = $response->original;
             if (is_a($original, 'Illuminate\View\View')) {
                 $path = $original->getPath();

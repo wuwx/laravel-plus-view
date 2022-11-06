cdb6ee4bacf63a18f6677202be828392c91f5fec
wuwx <wuweixin@gmail.com>
2022-11-06 22:02:22 +0800

    wip

--- tests/LaravelPlusViewTest.php
+++ tests/LaravelPlusViewTest.php
@@ -29,4 +29,22 @@ class LaravelPlusViewTest extends TestCase
         $this->get("/?_format=xml")->assertStatus(200);
         $this->get("/?_format=xml")->assertSee("xml");
     }
+
+    public function testAcceptJs()
+    {
+        $this->get("/", ["Accept" => "application/javascript"])->assertStatus(200);
+        $this->get("/", ["Accept" => "application/javascript"])->assertSee("js");
+    }
+
+    public function testAcceptJson()
+    {
+        $this->get("/",["Accept" => "application/json"])->assertStatus(200);
+        $this->get("/",["Accept" => "application/json"])->assertSee("json");
+    }
+
+    public function testAcceptXml()
+    {
+        $this->get("/", ["Accept" => "application/xml"])->assertStatus(200);
+        $this->get("/", ["Accept" => "application/xml"])->assertSee("xml");
+    }
 }
